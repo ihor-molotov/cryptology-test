3d6108648e0d831f012e8e2b16cf6fd9e0e29b84
Ihor Dzhavala <55876558+ihor-molotov@users.noreply.github.com>
2022-06-20 17:12:16 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
 curl -s https://raw.githubusercontent.com/ihor-molotov/cryptology-test/main/logo.sh | bash
-
-echo -e "\n\033[0;94m Процес установки....!"
-echo -e "\n\033[0;94m Процес установки....!"
-echo -e "\n\033[0;94m Процес установки....!"
-echo -e "\n\033[0;94m Процес установки....!"
-echo -e "\n\033[0;94m Процес установки....!"
-echo -e "\n\033[0;94m Процес установки....!"
-
-echo -e "\n\033[0;94m Нода успешно установлена!"
